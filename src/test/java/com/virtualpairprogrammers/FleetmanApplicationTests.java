e2e9bf8691d0861cab98f5e58829206173768953
krish shukla <krishan.shyam@gmail.com>
2020-05-25 21:51:56 +0100

    first commit

---
+++ src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+// this is comment - one more
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class FleetmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
